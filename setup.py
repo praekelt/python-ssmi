55b1425ec55b37901806af9e32c1a432d7b4d33d
Morgan Collett <morgan@praekelt.com>
2009-07-31 13:53:30 +0200

    Following some more of the buildout process - setup.py, buildout.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+        name = "python-ssmi",
+        version = "0.0.1",
+        url = 'http://github.com/praekelt/python-ssmi',
+        license = 'proprietary',
+        description = "Python module implementing SSMI for USSD and SMS.",
+        author = 'Morgan Collett',
+        packages = find_packages('src'),
+        package_dir = {'': 'src'},
+        install_requires = ['setuptools'],
+)

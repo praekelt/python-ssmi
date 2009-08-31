857484f3abe0a26bff957af6f73685adc656f4af
Morgan Collett <morgan@praekelt.com>
2009-08-31 20:19:01 +0200

    Version bump for first egg deployment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = "python-ssmi",
-        version = "0.0.1",
+        version = "0.0.2",
         url = 'http://github.com/praekelt/python-ssmi',
         license = 'proprietary',
         description = "Python module implementing SSMI for USSD and SMS.",

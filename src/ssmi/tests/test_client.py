f42ede65d4762e9620c470adf6e19dce1644e706
Simon Cross <hodgestar@gmail.com>
2012-11-15 17:34:12 +0200

    Add tests for concatenated and split lines.

--- src/ssmi/tests/test_client.py
+++ src/ssmi/tests/test_client.py
@@ -51,3 +51,17 @@ class SSMIClientTestCase(TestCase):
         self.assertEqual(list_0['phase'], '0')
         self.assertEqual(list_0['message'], '*156#')
         self.assertEqual(list_0['genfields'], {})
+
+    def test_multi_line_receive(self):
+        lines = ('SSMI,110,27831112222,1,0,*156#\r'
+                 'SSMI,110,27831112222,1,0,*157#\r')
+        self.ssmi_client.dataReceived(lines)
+        list_0, list_1 = self.callback_populated_list
+        self.assertEqual(list_0['message'], '*156#')
+        self.assertEqual(list_1['message'], '*157#')
+
+    def test_split_line_receive(self):
+        self.ssmi_client.dataReceived('SSMI,110,27831112222')
+        self.ssmi_client.dataReceived(',1,0,*156#\r')
+        [list_0] = self.callback_populated_list
+        self.assertEqual(list_0['message'], '*156#')

7c1b67774adebcd7919d9d8e6aefe2e01354425d
Simon Cross <hodgestar@gmail.com>
2012-11-16 09:33:57 +0200

    PEP8 clean-up of setup,py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "python-ssmi",
-        version = "0.0.4",
-        url = 'http://github.com/praekelt/python-ssmi',
-        license = 'proprietary',
-        description = "Python module implementing SSMI for USSD and SMS.",
-        author = 'Morgan Collett',
-        author_email = 'morgan@praekelt.com',
-        packages = find_packages('src'),
-        package_dir = {'': 'src'},
-        install_requires = ['setuptools'],
+    name="python-ssmi",
+    version="0.0.4",
+    url='http://github.com/praekelt/python-ssmi',
+    license='proprietary',
+    description="Python module implementing SSMI for USSD and SMS.",
+    author='Morgan Collett',
+    author_email='morgan@praekelt.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['setuptools'],
 )

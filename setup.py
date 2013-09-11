0396f1c2eb1268b5c6aa5066edd31991d8fb34ad
Simon de Haan <simon@praekeltfoundation.org>
2013-09-11 13:51:22 +0200

    standardizing setup.py & requirements.pip usage

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup, find_packages
 
+def listify(filename):
+    return filter(None, open(filename, 'r').read().split('\n'))
+
 setup(
     name="python-ssmi",
     version="0.0.7",
-    url='http://github.com/praekelt/python-ssmi',
+    url='http://github.com/praekelt/python-rm',
     license='BSD',
     description="Python module implementing SSMI for USSD and SMS.",
     author='Praekelt Foundation',
     author_email='dev@praekeltfoundation.org',
     packages=find_packages(),
-    install_requires=[
-        'Twisted',
-    ],
+    install_requires=listify('requirements.pip'),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
